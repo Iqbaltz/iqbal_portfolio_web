b1b9cc925976b7a465ba7e76115070d40e59534f
ACER <mhdiqbaltan@gmail.com>
2022-03-25 09:21:06 +0700

    fix form

--- components/pages/contact/Form.jsx
+++ components/pages/contact/Form.jsx
@@ -1,11 +1,36 @@
 import styles from "./Form.module.scss";
+import { useState } from "react";
 
 export default function Form() {
+  const [status, setStatus] = useState("");
+
+  const submitForm = (ev) => {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        setStatus("SUCCESS");
+      } else {
+        setStatus("ERROR");
+      }
+    };
+    xhr.send(data);
+  };
   return (
     <section className={styles.form}>
       <h1 className={styles.title}>{header.title}</h1>
       <p className={styles.subtitle}>{header.subtitle}</p>
-      <form action="">
+      <form
+        onSubmit={submitForm}
+        action="https://formspree.io/f/xdoppqnk"
+        method="POST"
+      >
         <div className={styles.flexInput}>
           <div className={styles.half}>
             <label htmlFor="name">Name</label>
@@ -20,7 +45,12 @@ export default function Form() {
           <label htmlFor="message">Message</label>
           <textarea name="Message" id="message" cols="30" rows="10"></textarea>
         </div>
-        <button type="submit">Send Message</button>
+        {status === "SUCCESS" ? (
+          <p>Thank you for contacting me!</p>
+        ) : (
+          <button type="submit">Send Message</button>
+        )}
+        {status === "ERROR" && <p>Oops! Something went wrong</p>}
       </form>
     </section>
   );
--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,11 @@
+import { NextSeo } from "next-seo";
 import Layout from "../components/layout";
 import { Form } from "../components/pages/contact";
 
 export default function Contact() {
   return (
     <Layout>
+      <NextSeo title={"Contact"} openGraph={{ title: "Contact" }} />
       <Form />
     </Layout>
   );
