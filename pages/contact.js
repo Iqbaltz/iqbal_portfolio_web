1a20d7b1a75e64b1ee0cc60f6d1b16ed43dbb223
ACER <mhdiqbaltan@gmail.com>
2022-03-27 22:55:29 +0700

    fix bug

--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,23 @@
 import { NextSeo } from "next-seo";
 import Layout from "../components/layout";
 import { Form } from "../components/pages/contact";
+import { getSocialItems } from "../libs/api";
 
-export default function Contact() {
+export default function Contact({ socialItems }) {
   return (
-    <Layout>
+    <Layout socials={socialItems}>
       <NextSeo title={"Contact"} openGraph={{ title: "Contact" }} />
       <Form />
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const socialItems = await getSocialItems();
+  return {
+    props: {
+      socialItems,
+    },
+    revalidate: 1,
+  };
 }
