1764908e0568c0ad09f39379207f57dc8591b38c
ACER <mhdiqbaltan@gmail.com>
2022-04-23 22:27:20 +0700

    quickfix minor styling

--- components/pages/home/Projects.jsx
+++ components/pages/home/Projects.jsx
@@ -1,17 +1,26 @@
 import styles from "./Projects.module.scss";
 import { urlFor } from "../../../libs/sanity";
 
-export default function Projects({data}) {
+export default function Projects({ data }) {
   return (
     <section className={styles.projects}>
       <h1 className={styles.title}>{heading.title}</h1>
-      <p className={styles.subtitle}>{heading.subtitle} <a href="mailto:mhdiqbaltan@gmail.com">Contact me</a></p>
+      <p className={styles.subtitle}>
+        {heading.subtitle} <a href="mailto:mhdiqbaltan@gmail.com">Contact me</a>
+      </p>
       <div className={styles.wrapper}>
-        {data.map(({image, caption, url},i) => (
+        {data.map(({ image, caption, url }, i) => (
           <div className={styles.project} key={i}>
             <img src={urlFor(image)} alt={caption} />
             <p className={styles.desc}>{caption}</p>
-            <a href={url} className={styles.linkBtn}>Visit Website</a>
+            <a
+              href={url}
+              className={styles.linkBtn}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Visit Website
+            </a>
           </div>
         ))}
       </div>
@@ -24,36 +33,3 @@ const heading = {
   subtitle:
     "Here are some things I've been working on recently, want to see more?",
 };
-
-const projects = [
-  {
-    img: "/images/projects/metif.png",
-    about: `A branding agency website with Blog's and Project's feature, and using headless CMS`,
-    link: "http://mejakreatif.com/",
-  },
-  {
-    img: "/images/projects/sbs.png",
-    about: `A company profile website, SBS Cosmetics is a multinational cosmetics contract manufacturer in Indonesia`,
-    link: "http://sbs-cosmetics.com/",
-  },
-  {
-    img: "/images/projects/seyma.png",
-    about: `A personal portfolio site, Seyma is a talented UI/UX designer`,
-    link: "https://www.seymaermis.com/",
-  },
-  {
-    img: "/images/projects/nibras.png",
-    about: `A personal portfolio site, Nibras is a software engineer`,
-    link: "https://nibras-muhsin.herokuapp.com/",
-  },
-  {
-    img: "/images/projects/agro.png",
-    about: `Company profile website with Blog's and Project's feature, and headless CMS`,
-    link: "http://agrosumatera.com/",
-  },
-  {
-    img: "/images/projects/flateria.png",
-    about: `My Personal Project, This app let you create your own color palette`,
-    link: "https://flateriauicolors.netlify.app/",
-  },
-];
