63b9fe7f17ec2acfa9d74d40150e0e94093f5709
ACER <mhdiqbaltan@gmail.com>
2022-04-25 20:39:08 +0700

    seo-again

--- next-seo.config.js
+++ next-seo.config.js
@@ -6,7 +6,12 @@ export default {
     url: "https://muhammadiqbal.tech/",
     title: "Iqbal's Portfolio Site | Frontend Web Developer",
     description: `Iqbal is a freelance and a full-stack web developer based in Indonesia with a passion for building digital services/stuff and He love solving real-life problems with code.`,
-    images: [{ url: "/android-chrome-512x512.png" }],
+    images: [
+      { url: "/android-chrome-512x512.png" },
+      {
+        url: "https://cdn.sanity.io/images/rkhcfbt7/production/77434737944ad330fadafd810c363324b32eb915-1618x1078.png?w=2000&fit=max&auto=format",
+      },
+    ],
     site_name: "Muhammad Iqbal - Portfolio",
   },
   twitter: {
