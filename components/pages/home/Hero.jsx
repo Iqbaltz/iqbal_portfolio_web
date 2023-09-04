f9bdde13353df359edb38bd8fa56da7410685aa0
iqbaltz <mhdiqbaltan@gmail.com>
2023-09-04 11:45:57 +0700

    fix update style

--- components/pages/home/Hero.jsx
+++ components/pages/home/Hero.jsx
@@ -1,16 +1,15 @@
-import styles from './Hero.module.scss';
-import { urlFor } from '../../../libs/sanity';
+import styles from "./Hero.module.scss";
+import { urlFor } from "../../../libs/sanity";
 
-
-export default function Hero({data}) {
+export default function Hero({ data }) {
   return (
     <section className={styles.hero}>
-        <h1 className={styles.title}>{data.title}</h1>
-        <p className={styles.subtitle}>{data.subtitle}</p>
-        <div className={styles.avatar}>
-            <img className={styles.ava} src={urlFor(data.avatar)} alt="avatar" />
-            <img className={styles.skl} src="/images/skl.svg" alt="skills" />
-        </div>
+      <h1 className={styles.title}>{data.title}</h1>
+      <p className={styles.subtitle}>{data.subtitle}</p>
+      <div className={styles.avatar}>
+        <img className={styles.ava} src={urlFor(data.avatar)} alt="avatar" />
+        <img className={styles.skl} src="/images/skl.svg" alt="skills" />
+      </div>
     </section>
-  )
+  );
 }
